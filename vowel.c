159b092fc5a2fcac764ac2ad0dcf8f824a840412
Zain-Yousuf <93894509+Zain-Yousuf@users.noreply.github.com>
2021-11-29 18:54:40 +0500

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int i,j;
+  printf("enter the value ");
+  scanf("%d",&j);
+  for(i=0;i<=10;i++)
+  for(j=1;j<=8;j++)
+ 
+ if(i==9){
+ printf("zain is inocent boy\n");
+ } 
+ printf("zain==,8\n");
+  
+  {while(20);}
+
+
+
+}
